9a25696486f0d71ef856e2a1c2f8ef3c72a8617d
Samsamdu86 <barre.sam@yahoo.com>
2024-05-28 09:46:33 +0200

    create api

--- main_code.py
+++ main_code.py
@@ -6,9 +6,30 @@ app = Flask(__name__,
             template_folder='.'
             )
 
+API_key = 913a3052e4518b0d4c14d67a6a54540e
+
+def get_api(lat,lon):
+    url = "https://api.openweathermap.org/data/2.5/weather?lat={" + lat + "}&lon={" + lon + "}&appid={" + API_key + "}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        print("GET successfull code 200")
+        # note that the response content is webpage-like
+        data_string = response.content.decode('utf-8')
+        json_data = json.loads(data_string)
+        return json.dumps(json_data, indent=2)
+
+    else:
+        print("erreur, ", response.status_code)
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+
+
 
 
 # Lancement du serveur
